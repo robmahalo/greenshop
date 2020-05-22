36fbf9d760038aa74dea967358bd329ecba25fd1
Robert Manalo <robertmanalo@me.com>
2020-05-21 22:59:32 -0400

    Connected Signup to MongoDB Atlas

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -12,3 +12,20 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
+// Import Mongoos
+const mongoose = require('mongoose');
+// Load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+
+// Database Connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+)
+.then(() => console.log('Database is Now Connected'))
+
+mongoose.connection.on('Error', err => {
+    console.log(`Database Connection Error: ${err.message}`)
+})
